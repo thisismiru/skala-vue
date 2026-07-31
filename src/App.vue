02b8a90a642b28a919b1f8076fcf4de57eb0c794
김성현 <iroi3930@gmail.com>
2026-07-31 14:37:55 +0900

    Vue 프로젝트 초기 구성 및 디자인 시스템, code Challenge 반영

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import SampleOne from './components/practices/basic/SampleOne.vue'
+import SampleTwo from './components/practices/basic/SampleTwo.vue'
+</script>
+
+<template>
+  <SampleOne />
+  <SampleTwo />
+</template>
