91ef80b5cdb7d17e4f5d11d7ef115698b38f606c
김성현 <iroi3930@gmail.com>
2026-07-31 17:50:23 +0900

    Day1 commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup>
+import DirectiveBind from './components/practices/basic/DirectiveBind.vue'
+import DirectiveClassBinding from './components/practices/basic/DirectiveClassBinding.vue'
+import DirectiveStyleBinding from './components/practices/basic/DirectiveStyleBinding.vue'
+import DirectiveHTML from './components/practices/basic/DirectiveHTML.vue'
+import DirectiveHTMLXSS from './components/practices/basic/DirectiveHTMLXSS.vue'
+import DirectiveText from './components/practices/basic/DirectiveText.vue'
 import SampleOne from './components/practices/basic/SampleOne.vue'
 import SampleTwo from './components/practices/basic/SampleTwo.vue'
+import DirectiveSameNameShorthand from './components/practices/basic/DirectiveSameNameShorthand.vue'
+import DirectiveIf from './components/practices/basic/DirectiveIf.vue'
 </script>
 
 <template>
   <SampleOne />
   <SampleTwo />
+  <DirectiveHTML />
+  <DirectiveHTMLXSS />
+  <DirectiveText />
+  <DirectiveBind />
+  <DirectiveClassBinding />
+  <DirectiveStyleBinding />
+  <DirectiveSameNameShorthand />
+  <DirectiveIf />
 </template>
