507bc7c22c8d6b3e2be1bd69c3d6ce90f636d9b0
김성현 <iroi3930@gmail.com>
2026-08-03 13:08:45 +0900

    Day2 오전강의 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import WeatherMockup from './components/exercise/WeatherMockup.vue'
 import DirectiveBind from './components/practices/basic/DirectiveBind.vue'
 import DirectiveClassBinding from './components/practices/basic/DirectiveClassBinding.vue'
 import DirectiveStyleBinding from './components/practices/basic/DirectiveStyleBinding.vue'
@@ -12,9 +13,28 @@ import DirectiveIf from './components/practices/basic/DirectiveIf.vue'
 import DirectiveShow from './components/practices/basic/DirectiveShow.vue'
 import DirectiveFor from './components/practices/basic/DirectiveFor.vue'
 import DirectivePre from './components/practices/basic/DirectivePre.vue'
+import DirectiveCloak from './components/practices/basic/DirectiveCloak.vue'
+import DirectiveOnce from './components/practices/basic/DirectiveOnce.vue'
+import DirectiveMemo from './components/practices/basic/DirectiveMemo.vue'
+import DirectiveOn from './components/practices/basic/DirectiveOn.vue'
+import EventHandlingEventObject from './components/practices/basic/EventHandlingEventObject.vue'
+import EventHandlingEventModifier from './components/practices/basic/EventHandlingEventModifier.vue'
+import FormDataBindingVModel from './components/practices/basic/FormDataBindingVModel.vue'
+import FormDataBindingFromElementsHandling from './components/practices/basic/FormDataBindingFromElementsHandling.vue'
+import FormDataBindingVModelModifiers from './components/practices/basic/FormDataBindingVModelModifiers.vue'
+import StyleExample from './components/practices/basic/StyleExample.vue'
+import ReactiveStateRef from './components/practices/composition-api/ReactiveStateRef.vue'
+import ReactiveStateReactive from './components/practices/composition-api/ReactiveStateReactive.vue'
+import ComputedWatchersComputed from './components/practices/composition-api/ComputedWatchersComputed.vue'
+import ComputedWatchersWatch from './components/practices/composition-api/ComputedWatchersWatch.vue'
+import ComputedWatchersMultiSourceWatch from './components/practices/composition-api/ComputedWatchersMultiSourceWatch.vue'
+import ComputedWatchersDeepWatch from './components/practices/composition-api/ComputedWatchersDeepWatch.vue'
+import ComputedWatchersReactiveWatch from './components/practices/composition-api/ComputedWatchersReactiveWatch.vue'
+import ComputedWatchersWatchEffect from './components/practices/composition-api/ComputedWatchersWatchEffect.vue'
 </script>
 
 <template>
+  <weatherMockup />
   <SampleOne />
   <SampleTwo />
   <DirectiveHTML />
@@ -28,4 +48,22 @@ import DirectivePre from './components/practices/basic/DirectivePre.vue'
   <DirectiveShow />
   <DirectiveFor />
   <DirectivePre />
+  <DirectiveCloak />
+  <DirectiveOnce />
+  <DirectiveMemo />
+  <DirectiveOn />
+  <EventHandlingEventObject />
+  <EventHandlingEventModifier />
+  <FormDataBindingVModel />
+  <FormDataBindingFromElementsHandling />
+  <FormDataBindingVModelModifiers />
+  <StyleExample />
+  <ReactiveStateRef />
+  <ReactiveStateReactive />
+  <ComputedWatchersComputed />
+  <ComputedWatchersWatch />
+  <ComputedWatchersMultiSourceWatch />
+  <ComputedWatchersDeepWatch />
+  <ComputedWatchersReactiveWatch />
+  <ComputedWatchersWatchEffect />
 </template>
